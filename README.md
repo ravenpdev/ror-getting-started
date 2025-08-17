a0df5a3f78ea96f2561459b426d88294ea26e6cd
raven paragas <ravenp.dev@gmail.com>
2025-08-17 12:09:38 +0800

    chore: getting started with rails

---
+++ README.md
@@ -0,0 +1,8 @@
+## Routes
+
+**Parts of a URL**
+
+- https is the **protocol**
+- exaple.org is the **host**
+- /products is the **path**
+- ?sale=true&sort=asc aret the **query parameters**
---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,56 @@
+class ProductsController < ApplicationController
+  allow_unauthenticated_access only: %i[index show]
+
+  before_action :set_product, only: %i[show edit update destroy]
+
+  def index
+    @products = Product.all.order(created_at: :desc)
+  end
+
+  def show
+    # @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    logger.debug "Product Params: #{product_params}"
+    logger.debug "Product: #{@product.attributes.inspect}"
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    # @product = Product.find(params[:id])
+  end
+
+  def update
+    # @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.expect(product: [ :name, :description, :featured_image, :inventory_count ])
+  end
+end
